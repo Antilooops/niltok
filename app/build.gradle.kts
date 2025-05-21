e41f507976c6173130d1502a7d011145e95d813e
Antilooops <louissavignac333@gmail.com>
2025-05-21 22:06:38 +0200

    feat: first version of implementing the API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("kotlin-parcelize")
 }
 
 android {
@@ -45,4 +47,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.squareup.retrofit2:retrofit:2.8.1")
+    implementation("com.squareup.retrofit2:converter-moshi:2.8.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.4.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
