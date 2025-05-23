07b7763d8ca888b72f3e88541273af93bcedcc8e
FantinLNA <fantin.ellena--mehl@epfedu.fr>
2025-05-23 17:13:49 +0200

    shop work with lateral menu

--- app/src/main/java/louis/app/niltok/activity/ShopActivity.kt
+++ app/src/main/java/louis/app/niltok/activity/ShopActivity.kt
@@ -10,10 +10,11 @@ import louis.app.niltok.R
 import louis.app.niltok.adapter.ProductAdapter
 import louis.app.niltok.model.Product
 
-class ShopActivity : AppCompatActivity() {
+class ShopActivity : NavActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shop)
+//        setContentView(R.layout.activity_shop)
+        layoutInflater.inflate(R.layout.activity_shop, findViewById(R.id.activity_container), true)
 
         val recyclerView = findViewById<RecyclerView>(R.id.activity_shop_recyclerview)
 
