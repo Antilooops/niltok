23b9fca575f2c926114cb07ed303a3389306a8b6
FantinLNA <fantin.ellena--mehl@epfedu.fr>
2025-05-28 18:40:38 +0200

    Search bar working and done

--- app/src/main/java/louis/app/niltok/activity/ShopActivity.kt
+++ app/src/main/java/louis/app/niltok/activity/ShopActivity.kt
@@ -2,6 +2,7 @@ package louis.app.niltok.activity
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.runBlocking
@@ -11,20 +12,38 @@ import louis.app.niltok.model.CartManager
 import louis.app.niltok.model.Product
 
 class ShopActivity : NavActivity()  {
+
+    private lateinit var adapter: ProductAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         layoutInflater.inflate(R.layout.activity_shop, findViewById(R.id.activity_container), true)
 
         val recyclerView = findViewById<RecyclerView>(R.id.activity_shop_recyclerview)
+        val searchView =findViewById<SearchView>(R.id.search_view)
 
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         runBlocking {
             val products = Product.getProduct()
-            recyclerView.adapter = ProductAdapter(products) { product ->
+            val adapter = ProductAdapter(products) { product ->
                 CartManager.addItem(product)
                 Toast.makeText(this@ShopActivity, "${product.title} added to cart", Toast.LENGTH_SHORT).show()
             }
+            recyclerView.adapter = adapter
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    adapter.filter(query.orEmpty())
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.filter(newText.orEmpty())
+                    return true
+                }
+            })
+
         }
     }
 }
