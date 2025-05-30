50fd5a0c01a11ea7379134be6cb83f6dd7ca6ff2
Antilooops <louissavignac333@gmail.com>
2025-05-30 13:56:41 +0200

    feat: total price in cart

--- app/src/main/java/louis/app/niltok/activity/CartActivity.kt
+++ app/src/main/java/louis/app/niltok/activity/CartActivity.kt
@@ -13,32 +13,23 @@ import louis.app.niltok.model.CartManager
 class CartActivity : NavActivity() {
     private lateinit var cartAdapter: CartAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
-        // super.onCreate(savedInstanceState)
-        // layoutInflater.inflate(R.layout.activity_cart, findViewById(R.id.activity_container), true)
 
-        // val recyclerView = findViewById<RecyclerView>(R.id.activity_cart_recyclerview)
-
-        // recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-        // runBlocking {
-        //     recyclerView.adapter = CartAdapter(CartManager.getCartItems(), this@CartActivity)
-        // }
         super.onCreate(savedInstanceState)
         layoutInflater.inflate(R.layout.activity_cart, findViewById(R.id.activity_container), true)
 
         val recyclerView = findViewById<RecyclerView>(R.id.activity_cart_recyclerview)
+        var totalPriceTextView = findViewById<TextView>(R.id.activity_cart_total_price_textview)
 
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-        // Initialize adapter with an empty list initially or the current cart items
-        // The LiveData observer will update it
         cartAdapter = CartAdapter(CartManager.getCartItems(), this@CartActivity)
         runBlocking {
             recyclerView.adapter = cartAdapter
-            // Observe changes in cartItemsLiveData
             CartManager.cartItemsLiveData.observe(this@CartActivity, Observer { updatedCartItems ->
-                // When cartItemsLiveData changes, submit the new list to the adapter
                 cartAdapter.submitList(updatedCartItems)
+            })
+            CartManager.cartTotalPrice.observe(this@CartActivity, Observer { totalPrice ->
+                totalPriceTextView.text = "Prix total: " + String.format("%.2f", totalPrice) + "€"
             })
         }
     }
